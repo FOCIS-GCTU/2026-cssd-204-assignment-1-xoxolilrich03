a0e75891a77eccf6a0e94848ebe6133b0234c383
xoxolilrich03 <xoxolilrich03@gmail.com>
2026-05-31 19:03:38 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Printing out my Initials
+# Assignment Number: 2
+#
+# Name: NYANTAKYI SAMUEL
+# STUDENT ID:  2425403124
+# Email: xoxolilrich03@gmail.com
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, NYANTAKYI SAMUEL, this programming assignment is my own work
+# and I have not provided this code to any other student
 
-def main():
-  pass #code goes here
+print(" ")  #Leaving a blank line
+
+print("...NSM")     #Declearing my Initials
+
+print(" ")
+def main():     #main function
+    #Starting with the Program
+    print("...N.........N......SSSSSSSSSS............MMMM.....MMMM...")
+    print("...NN........N.....SSSSSSSSSSS............MMMM.....MMMM...")
+    print("...N.N.......N....SSSS....................MM.MM...MM.MM...")
+    print("...N..N......N....SSSS....................MM.MM...MM.MM...")
+    print("...N....N....N.......SSSSSS...............MM.MM...MM.MM...")
+    print("...N.....N...N.........SSSSSS.............MM..MMMM...MM...")
+    print("...N......N..N.............SSSS...........MM..MMMM...MM...")
+    print("...N.......N.N.............SSSS...........MM...MM....MM...")
+    print("...N........NN...**.SSSSSSSSSSS.......**..MM...MM....MM.**")
+    print("...N........NN...**.SSSSSSSSSSS.......**..MM...MM....MM.**")
 
 
 
-main()
+if __name__ == "__main__":
+    main()
